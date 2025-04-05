3e1d55b4bb6d8866a6242121bfb284afb8858341
multilococ <multilococ@yandex.ru>
2025-04-05 10:29:11 +0300

    Fix

--- Assets/Scripts/TextChanger.cs
+++ Assets/Scripts/TextChanger.cs
@@ -18,9 +18,7 @@ public class TextChanger : MonoBehaviour
         Sequence sequence = DOTween.Sequence();
         
         sequence.Append(_text.DOText("Заменат текста", _duration));
-        sequence.SetRelative();
-        sequence.Append(_text.DOText(" дополнение к тексту", _duration));
-        sequence.SetRelative(false);
+        sequence.Append(_text.DOText(_text.text + " дополнение к тексту", _duration));
         sequence.Append(_text.DOText("Взламываем текст", _duration, true, ScrambleMode.All));
         sequence.SetLoops(-1,LoopType.Restart);
         sequence.SetDelay(_delay);
